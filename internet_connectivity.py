4a9cc67b9dc7ef89479a4accb8b38bed217522a0
Aleutian <33500695+Bering-Sea@users.noreply.github.com>
2021-05-06 15:31:48 -0800

    Update internet_connectivity.py

--- internet_connectivity.py
+++ internet_connectivity.py
@@ -1,17 +1,19 @@
+# My version of this app. 
+# any comments that are made starting off with *** are my comments. 
 import datetime
 import os
 import signal
 import socket
 import sys
 import time
-from smbus2 import SMBus # this one is important. You need to put from smbus2 and capitalize the SMB in import SMBus.
+from smbus2 import SMBus # *** this one is important. You need to put from smbus2 and capitalize the SMB in import SMBus.
 
-# I copied the example relay.py from wiki.52pi.com/index.php/DockerPi_4_Channel_Relay_SKU:_EP-0099#Customer_Application site.
-# It did not work for me when i tested it out. I had to some research and you'll notice some of the changes i made. 
+# *** I copied the example relay.py from wiki.52pi.com/index.php/DockerPi_4_Channel_Relay_SKU:_EP-0099#Customer_Application site.
+# *** It did not work for me when i tested it out. I had to some research and you'll notice some of the changes i made. 
 
-DEVICE_BUS = 1 # this from the wiki.52pi.com site. 
-DEVICE_ADDR = 0x10 # this address is with the switches both down. 
-bus = SMBus(DEVICE_BUS) # this is modified from the 52.pi site. I delete some wording to make this work.
+DEVICE_BUS = 1 # *** this from the wiki.52pi.com site. 
+DEVICE_ADDR = 0x10 # *** this address is with the switches both down. 
+bus = SMBus(DEVICE_BUS) # *** this is modified from the 52.pi site. I deleted some wording to make this work.
 
 def internet(host = "8.8.8.8", port = 53, timeout = 4):
     '''
@@ -64,20 +66,20 @@ def perpetual_connection_monitor(interval):
 
     while True:
         if internet():
-            #Here is where I added code to make the relay turn on and off. 
+            # *** Here is where I added code to make the relay turn on and off. 
             print("The internet is on")
             bus.write_byte_data(DEVICE_ADDR, 1, 0xFF)
-            #Notice the number 1? that means its using relay 1
+            # *** Notice the number 1? that means its using relay 1
             time.sleep(1)
             bus.write_byte_data(DEVICE_ADDR, 1, 0x00)
             time.sleep(1)
             bus.write_byte_data(DEVICE_ADDR, 2, 0xFF)
-            #obviously using relay 2. You can change any of these numbers or use 1 or all 4 relays.
+            # *** obviously using relay 2. You can change any of these numbers or use 1 or all 4 relays.
             time.sleep(1)
             bus.write_byte_data(DEVICE_ADDR, 2, 0x00)
             time.sleep(1)
             time.sleep(interval)
-            #relay 1 and 2 will turn on and off while the internet on.
+            # *** relay 1 and 2 will turn on and off while the internet on.
         else:
             total_downtime_seconds = 0
             downtime_start = datetime.datetime.now()
@@ -89,7 +91,7 @@ def perpetual_connection_monitor(interval):
 
             # Check every 'interval' seconds to see if the connection is back. Sum up the outage time in seconds by adding 'interval' to the total. 
             while not internet():
-                #Another spot I changed the code. This uses relay 3 and 4 as you can see. 
+                # *** Another spot I changed the code. This uses relay 3 and 4 as you can see. 
                 print("Lost connection, trying to reconnect.")
                 bus.write_byte_data(DEVICE_ADDR, 3, 0xFF)
                 time.sleep(1)
@@ -100,7 +102,7 @@ def perpetual_connection_monitor(interval):
                 bus.write_byte_data(DEVICE_ADDR, 4, 0x00)
                 time.sleep(1)
                 time.sleep(interval)
-                #relay 3 and 4 will turn on and off while the internet is down. 
+                # *** relay 3 and 4 will turn on and off while the internet is down. 
                 #total_downtime_seconds += interval
 
             # Log time of first subsequent succesful connection after a period offline:
